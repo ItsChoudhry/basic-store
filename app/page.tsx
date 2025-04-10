508ceaa8784408adef46797c562f3a32ee1adf39
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-04-10 23:23:45 +0100

    remove dumb name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <h1 className="text-4xl font-bold mb-6">Welcome to LLM Wrapper</h1>
+      <h1 className="text-4xl font-bold mb-6">Welcome to basic store</h1>
       <p className="text-lg mb-8">
         Unlock the power of language models—sign in to get started!
       </p>
--- app/sign-in/[[...signIn]]/page.tsx
+++ app/sign-in/[[...signIn]]/page.tsx
@@ -4,7 +4,7 @@ import { dark } from "@clerk/themes";
 export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blend-darken">
-      <h1 className="text-3xl font-bold mb-6">LLM Wrapper</h1>
+      <h1 className="text-3xl font-bold mb-6">basic store</h1>
       <SignIn
         routing="path"
         path="/sign-in"
